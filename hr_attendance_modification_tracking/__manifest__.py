747b90f8c956fc6581a64c4c2cae7ba13235a7e8
Saran440 <saranl@ecosoft.co.th>
2020-02-06 14:17:31 +0700

    [IMP] hr_attendance_modification_tracking: black, isort

--- hr_attendance_modification_tracking/__manifest__.py
+++ hr_attendance_modification_tracking/__manifest__.py
@@ -2,19 +2,14 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Hr Attendance Modification Tracking',
-    'summary': """
+    "name": "Hr Attendance Modification Tracking",
+    "summary": """
         Attendance changes will now be registered in the chatter.""",
-    'version': '12.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'Creu Blanca,Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/hr',
-    'depends': [
-        'hr_attendance',
-        'mail',
-    ],
-    'data': [
-        'views/hr_attendance_view.xml',
-    ],
-    'installable': True,
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Creu Blanca,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/hr",
+    "depends": ["hr_attendance", "mail"],
+    "data": ["views/hr_attendance_view.xml"],
+    "installable": True,
 }
