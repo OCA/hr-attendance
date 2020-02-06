56890fda03d5f0c5177873aa6a0964939eb67304
Saran440 <saranl@ecosoft.co.th>
2020-02-06 14:17:31 +0700

    [IMP] hr_attendance_modification_tracking: black, isort

--- hr_attendance_modification_tracking/models/hr_attendance.py
+++ hr_attendance_modification_tracking/models/hr_attendance.py
@@ -5,15 +5,9 @@ from odoo import fields, models
 
 
 class HrAttendance(models.Model):
-    _name = 'hr.attendance'
-    _inherit = ['hr.attendance', 'mail.thread']
+    _name = "hr.attendance"
+    _inherit = ["hr.attendance", "mail.thread"]
 
-    employee_id = fields.Many2one(
-        track_visibility='onchange'
-    )
-    check_in = fields.Datetime(
-        track_visibility='onchange'
-    )
-    check_out = fields.Datetime(
-        track_visibility='onchange'
-    )
+    employee_id = fields.Many2one(track_visibility="onchange")
+    check_in = fields.Datetime(track_visibility="onchange")
+    check_out = fields.Datetime(track_visibility="onchange")
