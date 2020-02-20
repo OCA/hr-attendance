bb5827224c00faf3529a30bdaaf52cadae97bf33
Enric Tobella <etobella@creublanca.es>
2020-02-20 11:00:52 +0100

    [13.0][FIX] theoretical_time: Context was not imported on wizard

--- hr_attendance_report_theoretical_time/__manifest__.py
+++ hr_attendance_report_theoretical_time/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Theoretical vs Attended Time Analysis",
-    "version": "13.0.1.0.0",
+    "version": "13.0.1.0.1",
     "category": "Human Resources",
     "website": "https://github.com/OCA/hr-attendance",
     "author": "Tecnativa, Odoo Community Association (OCA)",
