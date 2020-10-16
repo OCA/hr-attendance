ac552782262d13d3d11c7ef0deb5932b21ce5c94
David Alonso (Solvos) <david.alonso@solvos.es>
2020-10-16 18:49:35 +0200

    [MIG] hr_attendance_reason: Migration to 14.0
    [UPD] Update hr_attendance_reason.pot
    
    [UPD] README.rst

--- hr_attendance_reason/__manifest__.py
+++ hr_attendance_reason/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "HR Attendance Reason",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Human Resources",
     "website": "https://github.com/OCA/hr-attendance",
     "author": "Odoo S.A., Odoo Community Association (OCA)",
