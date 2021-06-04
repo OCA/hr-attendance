98c54811493a1ae56108e21905b0f693e165bfbc
meetkd <meet@entrivistech.com>
2021-06-04 17:12:34 +0530

    [IMP] hr_attendance_geolocation: black, isort, prettier

--- hr_attendance_geolocation/__manifest__.py
+++ hr_attendance_geolocation/__manifest__.py
@@ -6,10 +6,10 @@
     "summary": """
         With this module the geolocation of the user is tracked at the
         check-in/check-out step""",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "author": "ForgeFlow S.L.," "Odoo Community Association (OCA)",
-    "website": "https://github.com/OCA/hr",
+    "website": "https://github.com/OCA/hr-attendance",
     "depends": ["hr_attendance"],
     "data": [
         "views/assets.xml",
