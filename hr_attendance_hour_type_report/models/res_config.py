91daa3aea06beeda011c58095f3250c650ddf8e1
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2021-12-03 10:20:57 +0100

    14.0 [ADD] hr_attendance_hour_type_report
    This module provides a new report in hr_attendance to display the worked
    time of employees split in the following categories:
    
    * type of day (weekday, sunday, holiday)
    * daytime / nighttime
    
    The information of a day being a holiday comes from the configured
    public holidays from the module hr_holidays_public.
    
    This is meant as a helper for people interacting with external payroll systems.

---
+++ hr_attendance_hour_type_report/models/res_config.py
@@ -0,0 +1,12 @@
+from odoo import fields, models
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = "res.config.settings"
+
+    hr_night_work_hour_start = fields.Float(
+        related="company_id.hr_night_work_hour_start", readonly=False
+    )
+    hr_night_work_hour_end = fields.Float(
+        related="company_id.hr_night_work_hour_end", readonly=False
+    )
