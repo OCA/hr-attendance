b8a933a5b86348d0e0159649f2bd7c7a98508d1e
lubusax <lu.bu.sax@google.com>
2022-01-08 21:10:36 +0100

    [ADD] "attendance reason rfid" module

---
+++ hr_attendance_reason_rfid/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright (C) 2021 thingsintouch.com
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "HR Attendance Reason RFID",
+    "version": "13.0.1.0.0",
+    "category": "Human Resources",
+    "website": "https://github.com/OCA/hr-attendance",
+    "author": "thingsintouch.com, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["hr_attendance_reason"],
+    "data": [
+    ],
+}
