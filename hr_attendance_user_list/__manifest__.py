2cc51ed3301d9d182c031440fef2137fbdb346ac
Víctor Martínez <victor.martinez@tecnativa.com>
2022-03-02 14:32:12 +0100

    [ADD] hr_attendance_user_list: New addon. TT34897

---
+++ hr_attendance_user_list/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2022 Tecnativa - Víctor Martínez
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Hr Attendance User List",
+    "version": "13.0.1.0.0",
+    "category": "Human Resources",
+    "website": "https://github.com/OCA/hr-attendance",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": ["hr_attendance"],
+    "installable": True,
+    "data": ["views/hr_attendance_view.xml"],
+    "maintainers": ["victoralmau"],
+}
