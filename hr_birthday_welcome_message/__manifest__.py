072682b4443c4af1f2e25dfa0a4957da4cf75423
Alba Riera <alba.riera@creublanca.es>
2022-05-18 16:25:18 +0200

    [ADD] hr_birthday_welcome_message

---
+++ hr_birthday_welcome_message/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2022 Creu Blanca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Hr Birthday Welcome Message",
+    "summary": """
+        This addon adds a birthday message as welcome message
+        when it is the employee's birthday""",
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Creu Blanca,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/hr-attendance",
+    "depends": ["hr_attendance"],
+    "data": ["templates/assets.xml"],
+    "demo": [],
+}
