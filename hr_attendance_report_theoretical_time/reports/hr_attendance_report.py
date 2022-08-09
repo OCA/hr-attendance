6bbb8c0d362c1670c9dc62a41fdbe2e8083eb97c
Víctor Martínez <victor.martinez@tecnativa.com>
2022-08-09 08:19:51 +0200

    [IMP] hr_attendance_report_theoretical_time: Reduce code according to https://github.com/odoo/odoo/pull/97507/commits/19f3a8d0e6f658ba05a5feee75aa53ae76ef52ad. TT36461

--- hr_attendance_report_theoretical_time/reports/hr_attendance_report.py
+++ hr_attendance_report_theoretical_time/reports/hr_attendance_report.py
@@ -1,6 +1,6 @@
 # Copyright 2022 Tecnativa - Víctor Martínez
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from odoo import fields, models, tools
+from odoo import api, fields, models
 
 
 class HRAttendanceReport(models.Model):
@@ -8,26 +8,11 @@ class HRAttendanceReport(models.Model):
 
     theoretical_hours = fields.Float(readonly=True)
 
-    # pylint: disable=sql-injection,missing-return
-    def init(self):
-        """Inject worked_hours field in the query with this hack,fetching the query and
-        recreating it.
-        TODO: Simplify if merged https://github.com/odoo/odoo/pull/97507
-        """
-        tools.sql.drop_view_if_exists(self.env.cr, self._table)
-        super().init()
-        self.env.cr.execute("SELECT pg_get_viewdef(%s, true)", (self._table,))
-        view_def = self.env.cr.fetchone()[0]
-        view_def = view_def.replace(
-            "hra.worked_hours,", "hra.worked_hours, hra.theoretical_hours,"
-        )
-        view_def = view_def.replace(
-            "FROM hr_attendance", ", theoretical_hours FROM hr_attendance"
-        )
-        if view_def[-1] == ";":
-            view_def = view_def[:-1]
-        # Re-create view
-        tools.drop_view_if_exists(self.env.cr, self._table)
-        self.env.cr.execute(
-            "CREATE OR REPLACE VIEW %s AS ((%s))" % (self._table, view_def)
-        )
+    @api.model
+    def _select(self):
+        return super()._select() + """, hra.theoretical_hours"""
+
+    @api.model
+    def _from(self):
+        res = super()._from()
+        return res.replace("worked_hours", "worked_hours, theoretical_hours")
