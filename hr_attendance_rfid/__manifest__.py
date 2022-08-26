534097f671ddb6a02ed3664f8956cab0cd27b01d
lubusax <lu.bu.sax@gmail.com>
2022-08-26 17:55:58 +0200

    [MIG] hr_attendance_rfid: Migration to 15.0

--- hr_attendance_rfid/__manifest__.py
+++ hr_attendance_rfid/__manifest__.py
@@ -1,13 +1,14 @@
 # Copyright 2017 Comunitea Servicios Tecnológicos S.L.
 # Copyright 2018-19 ForgeFlow S.L. (https://www.forgeflow.com)
+# Copyright 2022 thingsintouch.com
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
     "name": "HR Attendance RFID",
-    "version": "14.0.1.1.0",
+    "version": "15.0.1.0.0",
     "category": "Human Resources",
     "website": "https://github.com/OCA/hr-attendance",
-    "author": "Comunitea, ForgeFlow, Odoo Community Association (OCA)",
+    "author": "Comunitea, ForgeFlow, thingsintouch, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
