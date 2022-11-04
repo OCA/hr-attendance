b5d2d35584504116bd20c35f0b8adfcc531384ac
Víctor Martínez <victor.martinez@tecnativa.com>
2022-11-04 10:32:22 +0100

    [MIG] hr_attendance_geolocation: Migration to 15.0
    TT40358

--- hr_attendance_geolocation/__manifest__.py
+++ hr_attendance_geolocation/__manifest__.py
@@ -6,14 +6,18 @@
     "summary": """
         With this module the geolocation of the user is tracked at the
         check-in/check-out step""",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
-    "author": "ForgeFlow S.L.," "Odoo Community Association (OCA)",
+    "author": "ForgeFlow S.L., Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/hr-attendance",
     "depends": ["hr_attendance"],
     "data": [
-        "views/assets.xml",
         "views/hr_attendance_views.xml",
         "data/location_data.xml",
     ],
+    "assets": {
+        "web.assets_backend": [
+            "hr_attendance_geolocation/static/src/js/attendance_geolocation.js",
+        ],
+    },
 }
