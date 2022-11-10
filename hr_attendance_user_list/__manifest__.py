2cba906f13ae01e4ff77b1ab114ec62cb2909bd6
Víctor Martínez <victor.martinez@tecnativa.com>
2022-11-10 15:56:08 +0100

    [MIG] hr_attendance_user_list: Migration to 15.
    TT40348

--- hr_attendance_user_list/__manifest__.py
+++ hr_attendance_user_list/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Hr Attendance User List",
-    "version": "13.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "Human Resources",
     "website": "https://github.com/OCA/hr-attendance",
     "author": "Tecnativa, Odoo Community Association (OCA)",
