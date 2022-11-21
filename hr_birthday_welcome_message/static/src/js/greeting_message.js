d0b1dae804995976fbbfd7fa6b19704a639501bb
Kevin Luna <kevin.luna@creublanca.es>
2022-11-21 12:42:52 +0100

    [IMP] hr_birthday_welcome_message: black, isort, prettier

--- hr_birthday_welcome_message/static/src/js/greeting_message.js
+++ hr_birthday_welcome_message/static/src/js/greeting_message.js
@@ -1,4 +1,4 @@
-odoo.define("hr_birhday_welcome_message.greeting_message_birthday_message", function(
+odoo.define("hr_birhday_welcome_message.greeting_message_birthday_message", function (
     require
 ) {
     "use strict";
@@ -8,17 +8,17 @@ odoo.define("hr_birhday_welcome_message.greeting_message_birthday_message", func
     var _t = core._t;
 
     greeting_message.include({
-        init: function(parent, action) {
+        init: function (parent, action) {
             this._super.apply(this, arguments);
             this.is_birthday = action.is_birthday;
         },
 
-        _get_birthday_message: function() {
+        _get_birthday_message: function () {
             return _t(
                 "Happy Birthday! <i class='fa fa-birthday-cake' /> <br/> Enjoy your day"
             );
         },
-        welcome_message: function() {
+        welcome_message: function () {
             this._super.apply(this, arguments);
             if (this.is_birthday) {
                 this.$(".o_hr_attendance_random_message").html(
