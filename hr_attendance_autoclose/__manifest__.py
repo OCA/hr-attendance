d142a76a445e6ffb2012415043b59f8a1bab227c
Alessandro Uffreduzzi <alessandro.uffreduzzi@pytech.it>
2023-04-05 12:56:03 +0200

    [MIG] hr_attendance_autoclose: Migration to 16.0

--- hr_attendance_autoclose/__manifest__.py
+++ hr_attendance_autoclose/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "HR Attendance Auto Close",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Human Resources",
     "summary": "Close stale Attendances",
     "website": "https://github.com/OCA/hr-attendance",
