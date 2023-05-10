cfe35d93b8109553d383df194f40f116c251e826
joblo-compassion <joel@compassion.ch>
2023-05-10 15:56:42 +0200

    [16.0] Add module hr_attendance_calendar_view

---
+++ hr_attendance_calendar_view/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2023 JumpTo
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Add calendar view to attendance, hr_attendance_calendar_view",
+    "summary": """
+        This module adds the calendar view as an option to display attendance""",
+    "license": "AGPL-3",
+    "author": "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/hr-attendance",
+    "category": "Human Resources",
+    "version": "16.0.1.0.0",
+    "depends": ["base", "hr_attendance"],
+    "data": [
+        "views/hr_attendance_calendar_views.xml",
+    ],
+}
