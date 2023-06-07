4399b6a2f67ed7d454b251b070b238890d6ca6bc
David <david.vidal@tecnativa.com>
2023-06-07 16:32:51 +0200

    [FIX] hr_attendance_reason: _rpc context overriding

--- hr_attendance_reason/static/src/js/my_attendances.esm.js
+++ hr_attendance_reason/static/src/js/my_attendances.esm.js
@@ -1,58 +1,66 @@
 /** @odoo-module **/
+import MyAttendances from "hr_attendance.my_attendances";
+import session from "web.session";
+import {_t} from "web.core";
 
-odoo.define("hr_attendance_reason.my_attendances", function (require) {
-    var MyAttendances = require("hr_attendance.my_attendances");
-    var core = require("web.core");
-    var _t = core._t;
-
-    MyAttendances.include({
-        willStart: function () {
-            const prom = this.call(
-                "hr_attendance_reason_service",
-                "updateUserContextAsync",
-                {
-                    extra_fields: [
-                        "show_reasons_on_attendance_screen",
-                        "required_reason_on_attendance_screen",
-                    ],
-                }
-            );
-            return Promise.all([prom, this._super.apply(this, arguments)]);
-        },
-        init: function () {
-            this._super.apply(this, arguments);
-            this.reasons = this.getAttendanceReasons();
-        },
-        getAttendanceReasons: function () {
-            this.reasons = [];
-            this._rpc({
-                model: "hr.attendance.reason",
-                method: "search_read",
-                fields: ["name", "action_type"],
-                domain: [["show_on_attendance_screen", "=", true]],
-            }).then((reasons) => {
-                this.reasons = reasons;
+MyAttendances.include({
+    willStart: function () {
+        Object.assign(session.user_context, {
+            extra_fields: [
+                "show_reasons_on_attendance_screen",
+                "required_reason_on_attendance_screen",
+            ],
+        });
+        return this._super();
+    },
+    init: function () {
+        this._super.apply(this, arguments);
+        this.reasons = this.getAttendanceReasons();
+    },
+    getAttendanceReasons: function () {
+        this.reasons = [];
+        this._rpc({
+            model: "hr.attendance.reason",
+            method: "search_read",
+            fields: ["name", "action_type"],
+            domain: [["show_on_attendance_screen", "=", true]],
+        }).then((reasons) => {
+            this.reasons = reasons;
+        });
+        return this.reasons;
+    },
+    // This override is the way to inject into the context of an enclosed rpc call.
+    _rpc: function () {
+        const [{model, method, context}] = arguments;
+        if (!context) {
+            return this._super(...arguments);
+        }
+        if (model === "hr.employee" && method === "search_read") {
+            context.extra_fields = [
+                "show_reasons_on_attendance_screen",
+                "required_reason_on_attendance_screen",
+            ];
+        }
+        if (this.attendance_reason_id) {
+            context.attendance_reason_id = this.attendance_reason_id;
+        }
+        return this._super(...arguments);
+    },
+    update_attendance: function () {
+        this.attendance_reason_id = parseInt(
+            this.$(".o_hr_attendance_reason").val(),
+            0
+        );
+        if (
+            this.attendance_reason_id === 0 &&
+            this.employee.required_reason_on_attendance_screen
+        ) {
+            this.displayNotification({
+                title: _t("Please, select a reason"),
+                type: "danger",
             });
-            return this.reasons;
-        },
-        update_attendance: function () {
-            const attendance_reason_id = parseInt(
-                this.$(".o_hr_attendance_reason").val(),
-                0
-            );
-            if (
-                attendance_reason_id === 0 &&
-                this.employee.required_reason_on_attendance_screen
-            ) {
-                this.do_warn(_t("Please, select a reason"));
-            } else {
-                const prom = this.call(
-                    "hr_attendance_reason_service",
-                    "updateUserContextAsync",
-                    {attendance_reason_id: attendance_reason_id}
-                );
-                return Promise.all([prom, this._super.apply(this, arguments)]);
-            }
-        },
-    });
+        } else {
+            this._super();
+        }
+    },
 });
