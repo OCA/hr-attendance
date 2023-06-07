35c60736069824d7e3599339b0c182dac3f752a2
David <david.vidal@tecnativa.com>
2023-06-07 16:32:51 +0200

    [FIX] hr_attendance_reason: _rpc context overriding

--- hr_attendance_reason/static/src/js/kiosk_confirm.esm.js
+++ hr_attendance_reason/static/src/js/kiosk_confirm.esm.js
@@ -1,64 +1,78 @@
 /** @odoo-module **/
+import KioskConfirm from "hr_attendance.kiosk_confirm";
+import {_t} from "web.core";
 
-odoo.define("hr_attendance_reason.kiosk_confirm", function (require) {
-    const KioskConfirm = require("hr_attendance.kiosk_confirm");
-    const session = require("web.session");
-    const core = require("web.core");
-    const _t = core._t;
-    const superSignInOut =
-        KioskConfirm.prototype.events["click .o_hr_attendance_sign_in_out_icon"];
-    const superPinPadButton =
-        KioskConfirm.prototype.events["click .o_hr_attendance_pin_pad_button_ok"];
-    KioskConfirm.include({
-        events: Object.assign({}, KioskConfirm.prototype.events, {
-            "click .o_hr_attendance_sign_in_out_icon": function () {
-                this.update_attendance(superSignInOut);
-            },
-            "click .o_hr_attendance_pin_pad_button_ok": function () {
-                this.update_attendance(superPinPadButton);
-            },
-        }),
-        willStart: async function () {
-            await this._super();
-            await this._rpc({
-                model: "hr.employee",
-                method: "search_read",
-                args: [
-                    [["user_id", "=", this.getSession().uid]],
-                    [
-                        "show_reasons_on_attendance_screen",
-                        "required_reason_on_attendance_screen",
-                    ],
+const superSignInOut =
+    KioskConfirm.prototype.events["click .o_hr_attendance_sign_in_out_icon"];
+const superPinPadButton =
+    KioskConfirm.prototype.events["click .o_hr_attendance_pin_pad_button_ok"];
+
+KioskConfirm.include({
+    events: Object.assign({}, KioskConfirm.prototype.events, {
+        "click .o_hr_attendance_sign_in_out_icon": function () {
+            this.update_attendance(superSignInOut);
+        },
+        "click .o_hr_attendance_pin_pad_button_ok": function () {
+            this.update_attendance(superPinPadButton);
+        },
+    }),
+    willStart: async function () {
+        await this._super();
+        await this._rpc({
+            model: "hr.employee",
+            method: "search_read",
+            args: [
+                [["user_id", "=", this.getSession().uid]],
+                [
+                    "show_reasons_on_attendance_screen",
+                    "required_reason_on_attendance_screen",
                 ],
-            }).then((res) => {
-                this.employee = res.length && res[0];
+            ],
+        }).then((res) => {
+            this.employee = res.length && res[0];
+        });
+        await this._rpc({
+            model: "hr.attendance.reason",
+            method: "search_read",
+            fields: ["name", "action_type"],
+            domain: [["show_on_attendance_screen", "=", true]],
+        }).then((reasons) => {
+            this.reasons = reasons;
+        });
+    },
+    // This override is the way to inject into the context of an enclosed rpc call.
+    _rpc: function () {
+        const [{model, method, context}] = arguments;
+        if (!context) {
+            return this._super(...arguments);
+        }
+        if (model === "hr.employee" && method === "search_read") {
+            context.extra_fields = [
+                "show_reasons_on_attendance_screen",
+                "required_reason_on_attendance_screen",
+            ];
+        }
+        if (this.attendance_reason_id) {
+            context.attendance_reason_id = this.attendance_reason_id;
+        }
+        return this._super(...arguments);
+    },
+    update_attendance: function (event_func) {
+        this.attendance_reason_id = parseInt(
+            this.$(".o_hr_attendance_reason").val(),
+            0
+        );
+        if (
+            this.attendance_reason_id === 0 &&
+            this.employee.required_reason_on_attendance_screen
+        ) {
+            this.displayNotification({
+                title: _t("Please, select a reason"),
+                type: "danger",
             });
-            await this._rpc({
-                model: "hr.attendance.reason",
-                method: "search_read",
-                fields: ["name", "action_type"],
-                domain: [["show_on_attendance_screen", "=", true]],
-            }).then((reasons) => {
-                this.reasons = reasons;
-            });
-        },
-        update_attendance: function (event_func) {
-            const attendance_reason_id = parseInt(
-                this.$(".o_hr_attendance_reason").val(),
-                0
-            );
-            Object.assign(session.user_context, {
-                attendance_reason_id: attendance_reason_id,
-            });
-            if (
-                attendance_reason_id === 0 &&
-                this.employee.required_reason_on_attendance_screen
-            ) {
-                this.do_warn(_t("Please, select a reason"));
-            } else {
-                event_func = event_func.bind(this);
-                event_func();
-            }
-        },
-    });
+        } else {
+            event_func = event_func.bind(this);
+            event_func();
+        }
+    },
 });
