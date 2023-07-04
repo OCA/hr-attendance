fdadcc7e09f4cb6236c9185ce277455f4d5329eb
mymage <stefano.consolaro@mymage.it>
2023-07-04 12:14:57 +0000

    Added translation using Weblate (Italian)
    Translated using Weblate (Italian)
    
    Currently translated at 100.0% (23 of 23 strings)
    
    Translation: hr-attendance-15.0/hr-attendance-15.0-hr_attendance_reason
    Translate-URL: https://translation.odoo-community.org/projects/hr-attendance-15-0/hr-attendance-15-0-hr_attendance_reason/it/
    
    [UPD] Update hr_attendance_reason.pot
    
    [UPD] README.rst
    
    hr_attendance_reason 15.0.1.2.0
    
    Update translation files
    
    Updated by "Update PO files to match POT (msgmerge)" hook in Weblate.
    
    Translation: hr-attendance-15.0/hr-attendance-15.0-hr_attendance_reason
    Translate-URL: https://translation.odoo-community.org/projects/hr-attendance-15-0/hr-attendance-15-0-hr_attendance_reason/

--- hr_attendance_reason/__manifest__.py
+++ hr_attendance_reason/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "HR Attendance Reason",
-    "version": "15.0.1.1.0",
+    "version": "15.0.1.2.0",
     "category": "Human Resources",
     "website": "https://github.com/OCA/hr-attendance",
     "author": "Odoo S.A., Tecnativa, Odoo Community Association (OCA)",
