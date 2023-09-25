75b333b8312b8685eaa318757ac9969ccc8b43e3
David Alonso <david.alonso@factorlibre.com>
2023-09-25 16:15:52 +0200

    [MIG] hr_attendance_modification_tracking: Migration to 16.0

--- hr_attendance_modification_tracking/__manifest__.py
+++ hr_attendance_modification_tracking/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    "name": "HR Attendance Modification Tracking",
+    "name": "HR Attendance Modification Tracking.",
     "summary": """
         Attendance changes will now be registered in the chatter.""",
     "version": "14.0.1.0.4",
