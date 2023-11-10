fb82f26cf08dbdaf6d439ef78f9b162bcf6e536b
Janik von Rotz <login@janikvonrotz.ch>
2023-11-10 14:39:09 +0100

    all: lint modules

--- hr_employee_attendance_report/report/hr_employee_report.py
+++ hr_employee_attendance_report/report/hr_employee_report.py
@@ -1,17 +1,15 @@
-from odoo import api, fields, models, _
 import logging
+
+from odoo import api, models
+
 _logger = logging.getLogger(__name__)
-import pytz
-from dateutil.relativedelta import relativedelta
-from datetime import datetime, date, timedelta, time
-from odoo.osv import expression
 
 from . import attendance_data
 
-class ReportHrEmployee(models.AbstractModel):
-    _name = 'report.hr_employee_attendance_report.hr_employee'
-    _description = 'Attendance and leave report'
 
+class ReportHrEmployee(models.AbstractModel):
+    _name = "report.hr_employee_attendance_report.hr_employee"
+    _description = "Attendance and leave report"
 
     @api.model
     def _get_report_values(self, docids, data=None):
--- hr_employee_attendance_report/wizard/select_period.py
+++ hr_employee_attendance_report/wizard/select_period.py
@@ -1,31 +1,45 @@
-import time
-from dateutil.relativedelta import relativedelta
-from datetime import datetime, timedelta, date
-from odoo import api, fields, models
 import logging
+from datetime import date, timedelta
+
+from dateutil.relativedelta import relativedelta
+
+from odoo import fields, models
+
 _logger = logging.getLogger(__name__)
 
 
 class HREmployeeAttendanceReportSelectPeriod(models.TransientModel):
 
-    _name = 'hr_employee_attendance_report.select_period'
-    _description = 'Attendance and leave report period selector'
+    _name = "hr_employee_attendance_report.select_period"
+    _description = "Attendance and leave report period selector"
 
-    date_from = fields.Date(string='From', required=True, default=lambda *a: str(date.today() + relativedelta(months=-1, day=1)))
-    date_until = fields.Date(string='Until', required=True, default=lambda *a: str(date.today() + relativedelta(day=1) - timedelta(days=1)))
+    date_from = fields.Date(
+        string="From",
+        required=True,
+        default=lambda *a: str(date.today() + relativedelta(months=-1, day=1)),
+    )
+    date_until = fields.Date(
+        string="Until",
+        required=True,
+        default=lambda *a: str(date.today() + relativedelta(day=1) - timedelta(days=1)),
+    )
 
     def print_report(self, download_only=False):
         self.ensure_one()
-        context = self.env.context        
+        context = self.env.context
 
         # Check if wizards is called from user or employee view
-        active_model = context.get('active_model')
-        report = 'hr_employee_attendance_report.hr_employee_report'
-        if active_model == 'res.users':
-            report = 'hr_employee_attendance_report.res_users_report'
+        active_model = context.get("active_model")
+        report = "hr_employee_attendance_report.hr_employee_report"
+        if active_model == "res.users":
+            report = "hr_employee_attendance_report.res_users_report"
 
-        [data] = self.read()        
-        action = self.env.ref(report).with_context(download_only=download_only).report_action(None, data)
+        [data] = self.read()
+        action = (
+            self.env.ref(report)
+            .with_context(download_only=download_only)
+            .report_action(None, data)
+        )
         return action
 
     def download_report(self):
