b403b144e1d594d1251fb7d64a4697975f98153a
Jairo Llopis <yajo.sk8@gmail.com>
2024-07-23 13:05:29 +0100

    [FIX] hr_contract_update_overtime: tests compatible with hr_holidays_attendance
    If you run this module's tests while it's being installed for the 1st time, `hr_holidays_attendance` will enter its test scope and add extra constrains to which leaves are considered for employees. If we don't follow those constrains, the tests fail.
    
    Thus here I'm using that module lazily, while not being strictly required as a dependency. If installed, I fulfill the data required for it to work, and then tests are more resilient.
    
    @moduon MT-6583

--- hr_contract_update_overtime/models/hr_contract_history.py
+++ hr_contract_update_overtime/models/hr_contract_history.py
@@ -8,19 +8,17 @@ class HrContractHistory(models.Model):
 
     def action_update_overtime(self):
         for record in self:
-            all_contracts = record.mapped("contract_ids").sorted(
-                "date_start", reverse=False
-            )
+            all_contracts = record.contract_ids.sorted("date_start", reverse=False)
             valid_contracts = all_contracts.filtered(
                 lambda c: c.state in {"open", "close"}
             )
             for contract in valid_contracts:
                 other_contracts = all_contracts - contract
                 # Reorganize Leaves
-                other_contracts.mapped("resource_calendar_id").transfer_leaves_to(
+                other_contracts.resource_calendar_id.transfer_leaves_to(
                     contract.resource_calendar_id,
                     resources=contract.employee_id.resource_id,
                     from_date=contract.date_start,
                 )
-            # Update Overtime
-            all_contracts.action_update_overtime()
+        # Update Overtime
+        self.contract_ids.action_update_overtime()
