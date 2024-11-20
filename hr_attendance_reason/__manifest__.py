1df4f1281c24d6a4979315c7b9b0eb9ee405d515
Víctor Martínez <victor.martinez@tecnativa.com>
2024-11-20 09:39:42 +0100

    [IMP] hr_attendance_reason: Change reasons to define them as demo data
    TT51793

--- hr_attendance_reason/__manifest__.py
+++ hr_attendance_reason/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "HR Attendance Reason",
-    "version": "17.0.1.0.0",
+    "version": "17.0.1.1.0",
     "category": "Human Resources",
     "website": "https://github.com/OCA/hr-attendance",
     "author": "Odoo S.A., Tecnativa, Odoo Community Association (OCA)",
@@ -12,12 +12,14 @@
     "installable": True,
     "depends": ["hr_attendance"],
     "data": [
-        "data/hr_attendance_reason_data.xml",
         "security/ir.model.access.csv",
         "security/security.xml",
         "views/hr_attendance_reason_view.xml",
         "views/hr_attendance_view.xml",
         "views/res_config_settings_view.xml",
+    ],
+    "demo": [
+        "demo/hr_attendance_reason_demo.xml",
     ],
     "assets": {
         "web.assets_backend": [
