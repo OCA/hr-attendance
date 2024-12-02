68fc3b90e926578d94001e8f6cceff01c8760681
Tom Tietze <tom.tietze@manatec.de>
2024-12-02 14:16:19 +0100

    [MIG] hr_attendance_reason: Migration to 18.0

--- hr_attendance_reason/controllers/main.py
+++ hr_attendance_reason/controllers/main.py
@@ -1,6 +1,7 @@
 # Copyright 2024 Tecnativa - Carlos Lopez
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
+from odoo import http
 from odoo.http import request, route
 
 from odoo.addons.hr_attendance.controllers.main import HrAttendance
@@ -11,7 +12,23 @@ class HrAttendance(HrAttendance):
     @route("/hr_attendance/attendance_user_data", type="json", auth="user")
     def user_attendance_data(self):
         response = super().user_attendance_data()
-        response.update(self._get_attendance_reason_settings(request.env.company))
+        # try to get the company of the employee to show the correct reasons from
+        # the employees company
+        company = (
+            request.env.user.employee_id.company_id
+            if request.env.user.employee_id
+            else request.env.company
+        )
+        response.update(self._get_attendance_reason_settings(company))
+        # get available reasons for employee company
+        reasons = []
+        if response.get("attendance_state", False):
+            if response.get("attendance_state", False) == "checked_out":
+                action_type = "sign_in"
+            else:
+                action_type = "sign_out"
+            reasons = self._get_attendance_reasons(action_type, company)
+        response.update({"reasons": reasons})
         return response
 
     @route("/hr_attendance/systray_check_in_out", type="json", auth="user")
@@ -22,13 +39,17 @@ class HrAttendance(HrAttendance):
             )
         return super().systray_attendance(latitude=latitude, longitude=longitude)
 
-    @route("/hr_attendance/manual_selection", type="json", auth="public")
-    def manual_selection(self, token, employee_id, pin_code):
+    @http.route("/hr_attendance/manual_selection", type="json", auth="public")
+    def manual_selection_with_geolocation(
+        self, token, employee_id, pin_code, latitude=False, longitude=False
+    ):
         if request.params.get("attendance_reason_id"):
             request.update_context(
                 attendance_reason_id=request.params.get("attendance_reason_id")
             )
-        return super().manual_selection(token, employee_id, pin_code)
+        return super().manual_selection_with_geolocation(
+            token, employee_id, pin_code, latitude, longitude
+        )
 
     # new routes
     @route("/hr_attendance_reason/get_reasons", type="json", auth="public")
@@ -64,9 +85,17 @@ class HrAttendance(HrAttendance):
     def _get_attendance_reason_settings(self, company):
         show_reason = company.show_reason_on_attendance_screen
         required_reason = company.required_reason_on_attendance_screen
+        default_sign_in_reason_id = (
+            company.reason_on_attendance_screen_default_sign_in.id
+        )
+        default_sign_out_reason_id = (
+            company.reason_on_attendance_screen_default_sign_out.id
+        )
         return {
             "show_reason_on_attendance_screen": show_reason,
             "required_reason_on_attendance_screen": required_reason,
+            "default_sign_in_reason_id": default_sign_in_reason_id,
+            "default_sign_out_reason_id": default_sign_out_reason_id,
         }
 
     def _get_attendance_reasons(self, action_type, company):
