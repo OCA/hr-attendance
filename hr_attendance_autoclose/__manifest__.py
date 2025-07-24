f549856592274cdcc3b7f4b7e2979fc322286eab
Kimkhoi3010 <kkimkhoi3010@gmail.com>
2025-07-24 18:40:09 +0700

    [MIG] hr_attendance_autoclose: Migration to 18.0

--- hr_attendance_autoclose/__manifest__.py
+++ hr_attendance_autoclose/__manifest__.py
@@ -10,13 +10,8 @@
     "author": "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "installable": True,
-    "depends": ["hr_attendance_reason"],
+    "depends": ["hr_attendance"],
     "data": [
-        "data/hr_attendance_reason.xml",
-        "data/hr_attendance.xml",
-        "data/res_company.xml",
-        "views/hr_attendance_view.xml",
-        "views/hr_employee.xml",
         "views/res_config_settings_view.xml",
     ],
 }
