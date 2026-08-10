25de698aad4a373af04822389dcead3cfd082de7
khoi <kkimkhoi3010@gmail.com>
2026-08-10 10:31:14 +0700

    [IMP] hr_attendance_autoclose: rename to hr_attendance_flexible_autoclose

--- hr_attendance_flexible_autoclose/__manifest__.py
+++ hr_attendance_flexible_autoclose/__manifest__.py
@@ -2,10 +2,10 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
-    "name": "HR Attendance Auto Close",
+    "name": "HR Attendance Flexible Auto Close",
     "version": "18.0.1.0.0",
     "category": "Human Resources",
-    "summary": "Close stale Attendances",
+    "summary": "Close stale Attendances for employees with Flexible Hours",
     "website": "https://github.com/OCA/hr-attendance",
     "author": "Odoo Community Association (OCA)",
     "license": "AGPL-3",
