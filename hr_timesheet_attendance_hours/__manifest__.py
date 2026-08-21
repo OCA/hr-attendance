a1fde3b8cccb31a7ac3c64dee933701ee9f718a3
Marcos Mendez <mendez.foto@gmail.com>
2026-08-21 13:27:20 -0300

    [ADD] hr_timesheet_attendance_hours: attendance hours on timesheet lines
    Shows, on each timesheet line, the hours actually clocked by the employee
    on that day, so the recorded work can be compared against the attendance
    coming from the turnstile.

---
+++ hr_timesheet_attendance_hours/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Timesheet - Attendance Hours",
+    "version": "16.0.1.0.0",
+    "category": "Human Resources/Attendances",
+    "summary": "Show employee attendance hours on timesheet lines",
+    "author": "Pop Solutions, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/hr-attendance",
+    "license": "AGPL-3",
+    "depends": ["hr_timesheet", "hr_attendance"],
+    "data": ["views/account_analytic_line_views.xml"],
+    "installable": True,
+    "auto_install": False,
+}
